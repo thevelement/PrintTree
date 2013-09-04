2676ed2f9cb3dde043114485fef9144bafabb613
thevelement <vinny.santa.maria@gmail.com>
2013-09-04 13:34:03 -0500

    Added more comments

--- PrintTree.php
+++ PrintTree.php
@@ -12,6 +12,7 @@ class PrintTree {
     	$out = print_r($data, true);
 		
 		if ($this->jquery) {
+			// instead of using javascript in the 'href' attribute, we'll simply add a class to the A and DIV tags and let jQuery do the rest
 			$out = preg_replace('/([ \t]*)(\[[^\]]+\][ \t]*\=\>[ \t]*[a-z0-9 \t_]+)\n[ \t]*\(/iUe', "'\\1<a class=\"content_anchor\" href=\"#\">\\2</a><div class=\"content\" style=\"display: none;\">'", $out);
 		} else {		
 			// replace something like '[element] => <newline> (' with <a href="javascript:toggleDisplay('...');">...</a><div id="..." style="display: none;">
@@ -26,7 +27,8 @@ class PrintTree {
 		$out = substr_replace($out, "\n)", $last_div_position, 6);	
 		
 		if ($this->jquery) {
-			$output = '<script type="text/javascript">$(function() { $("body").on("click", "a.content_anchor", function(event) { $(this).next("div.content").toggle(); return false;}); });</script>' . "\n<pre>" . $out . '</pre>';
+			// we'll create the immediately invoked function and bind a click event to BODY. In order to cancel the click event on the A tag, we'll return FALSE in the jQuery event handler
+			$output = '<script type="text/javascript">(function($) { $(this).next("div.content").toggle(); return false; }); }(jQuery));</script>' . "\n<pre>" . $out . '</pre>';
 		} else {
 			$output = '<script type="text/javascript">function toggleDisplay(id) { document.getElementById(id).style.display = (document.getElementById(id).style.display == "block") ? "none" : "block"; }</script>' . "\n<pre>" . $out . '</pre>';						
 		}
